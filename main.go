42b128ba22fe4061722761061c6eae21aeff3e83
goraxe <goraxe@goraxe.me.uk>
2011-06-01 00:20:03 -0700

    more logging, more error handling, refactor AddInput

--- main.go
+++ main.go
@@ -10,73 +10,91 @@ import (
 
 func main() {
     // create a network with two inputs one output
-    fmt.Printf("Creating network\n")
     ann :=  network.CreateNetwork(2, 1)
+    ann.SetLogLevel(0)
 
-    fmt.Printf("\tCreating neuron1\n")
-    neuron1 := ann.CreateNeuron("neuron1", 1, 1.0)
+    neuron1 := ann.CreateNeuron("neuron1", 1.0)
+//    neuron1.SetLogLevel(3)
     ann.AddInput(0, neuron1, 1)
 
-    fmt.Printf("\tCreating neuron2\n")
-    neuron2 := ann.CreateNeuron("neuron2", 1, 1.0)
+    neuron2 := ann.CreateNeuron("neuron2", 1.0)
+//    neuron2.SetLogLevel(3)
     ann.AddInput(1, neuron2, 1)
     
-    fmt.Printf("\tCreating neuron3\n")
-    neuron3 := ann.CreateNeuron("neuron3", 2, 1.0)
+    neuron3 := ann.CreateNeuron("neuron3", 1.0)
+//    neuron3.SetLogLevel(3)
     neuron3.AddInput(neuron1, 1)
     neuron3.AddInput(neuron2, -1)
 
-    fmt.Printf("\tCreating neuron4\n")
-    neuron4 := ann.CreateNeuron("neuron4", 2, 1.0)
+    neuron4 := ann.CreateNeuron("neuron4", 1.0)
+//    neuron4.SetLogLevel(3)
     neuron4.AddInput(neuron1, -1)
     neuron4.AddInput(neuron2, 1)
 
-    fmt.Printf("\tCreating neuron5\n")
-    neuron5 := ann.CreateNeuron("neuron5", 2, 1.0)
+    neuron5 := ann.CreateNeuron("neuron5", 1.0)
+//    neuron5.SetLogLevel(3)
     neuron5.AddInput(neuron3, 1)
     neuron5.AddInput(neuron4, 1)
 
     output1 := ann.Output(0)
     neuron5.AddOutput(output1)
 
-    pattern1 := network.MakePattern(0,0)
     var (
         output *vector.Vector
+        expected *vector.Vector
         e os.Error
     )
+    pattern1 := network.MakePattern(0,0)
+    expected = network.MakePattern(0)
+
     output, e = ann.RunPattern(pattern1)
     if(e == os.EINVAL) {
         os.Exit(1)
     }
-    for i := 0; i < output.Len(); i++ {
-        fmt.Printf("output%v = %v\n", i+1, output.At(i))
+    if(network.CompareVector(expected, output)) {
+        fmt.Printf("ok: expected output pattern 1\n")
+    } else {
+        fmt.Printf("fail: expected output pattern 1 got: %v expected %v\n", output, expected)
     }
 
     pattern2 := network.MakePattern(0,1)
+    expected = network.MakePattern(1)
+
     output, e = ann.RunPattern(pattern2)
     if(e == os.EINVAL) {
         os.Exit(1)
     }
-    for i := 0; i < output.Len(); i++ {
-        fmt.Printf("output%v = %v\n", i+1, output.At(i))
+    if(network.CompareVector(expected, output)) {
+        fmt.Printf("ok: expected output pattern 2\n")
+    } else {
+        fmt.Printf("fail: expected output pattern 2 got: %v expected %v\n", output, expected)
     }
 
     pattern3 := network.MakePattern(1,0)
+    expected = network.MakePattern(1)
+
+
     output, e = ann.RunPattern(pattern3)
     if(e == os.EINVAL) {
         os.Exit(1)
     }
-    for i := 0; i < output.Len(); i++ {
-        fmt.Printf("output%v = %v\n", i+1, output.At(i))
+    if(network.CompareVector(expected, output)) {
+        fmt.Printf("ok: expected output pattern 3\n")
+    } else {
+        fmt.Printf("fail: expected output pattern 3 got: %v expected %v\n", output, expected)
     }
 
     pattern4 := network.MakePattern(1,1)
+    expected = network.MakePattern(0)
+
     output, e = ann.RunPattern(pattern4)
     if(e == os.EINVAL) {
         os.Exit(1)
     }
-    for i := 0; i < output.Len(); i++ {
-        fmt.Printf("output%v = %v\n", i+1, output.At(i))
+    if(network.CompareVector(expected, output)) {
+        fmt.Printf("ok: expected output pattern 4\n")
+    } else {
+        fmt.Printf("fail: expected output pattern 4 got: %v expected %v\n", output, expected)
     }
 }
 
--- network.go
+++ network.go
@@ -26,6 +26,7 @@ type Network struct  {
     inputs  ConnectionArray // for presenting a pattern to the network
     outputs ConnectionArray // for collecting the results
     command map[string] chan message // for sending a command to all neurons
+    log_level int
 }
 
 /* the real neuron inside a goroutine */
@@ -48,7 +49,7 @@ type Neuron struct {
 }
 
 func newNetwork() *Network {
-    return &Network{ command: make (map[string] chan message)}
+    return &Network{ command: make (map[string] chan message), log_level: 1}
 }
 
 func newConnection(w Data) *Connection {
@@ -56,8 +57,8 @@ func newConnection(w Data) *Connection {
 }
 
 func newNeuron(name string,  outputs *vector.Vector, 
-               command chan message, inputs int, threshold Data) *neuron {
-    return &neuron{name: name, input: make(chan Data, NCPU) , outputs: outputs, command: command , inputs: inputs, inputs_waiting: inputs, threshold: threshold}
+               command chan message, threshold Data) *neuron {
+    return &neuron{name: name, input: make(chan Data, NCPU) , outputs: outputs, command: command , threshold: threshold}
 }
 
 func newConnectionArray(connections int) *ConnectionArray {
@@ -81,12 +82,26 @@ func MakePattern(a...Data) (*vector.Vector){
     return pattern
 }
 
+func CompareVector(v1 *vector.Vector, v2 *vector.Vector) bool {
+    if (v1.Len() != v2.Len()) {
+        return false
+    }
+    for i := 0; i < v1.Len(); i++ {
+        if v1.At(i) != v2.At(i) {
+            return false
+        }
+    }
+    return true
+}
+
 func (v *ConnectionArray) Len() int{
     return len(*v)
 }
 
 func CreateNetwork(inputs int, outputs int) *Network {
+    
     n := newNetwork()
+    n.debug("Creating network\n")
     n.inputs = *newConnectionArray(inputs) 
     n.outputs = *newConnectionArray(outputs)
 
@@ -96,22 +111,31 @@ func CreateNetwork(inputs int, outputs int) *Network {
     return n
 }
 
+func (network *Network) LogLevel() int {
+    return network.log_level
+}
+
+func (network *Network) SetLogLevel(level int)  {
+    network.log_level = level
+}
+
 func (network *Network) RunPattern(input *vector.Vector) (output *vector.Vector, err os.Error) {
 
-     fmt.Printf("resetting network\n");
+     network.debug("resetting network\n");
      ch := make(chan string)
      for _, v := range network.command {
          v <- message{ message: "Reset", reply: ch } // send reset
          // wait for response
          _ = <-ch
      }
+     // make sure pattern is same size as inputs
      if (input.Len() != network.inputs.Len() ) {
         return nil, os.EINVAL
      }
-     fmt.Printf("presenting pattern to network\n")
+     network.info("presenting pattern %v to network\n", *input)
      for i := 0; i < input.Len(); i++  {
          v := input.At(i).(Data)
-         fmt.Printf("\tsending %v to input %v\n", v, i)
+         network.trace("\tsending %v to input %v\n", v, i)
          network.inputs[i].c <- v * network.inputs[i].weight
      }
      output = new(vector.Vector)
@@ -119,12 +143,16 @@ func (network *Network) RunPattern(input *vector.Vector) (output *vector.Vector,
         o:=  <-network.outputs[i].c
         output.Push(o)
      }
+     network.info(" \\- output is %v\n", *output)
      return output, err
 }
 
 
 func (network *Network) AddInput(number int, neuron *Neuron, weight Data ) {
     network.inputs[number] = &Connection{ c: neuron.input, weight: weight }
+    ch := make(chan string)
+    neuron.command <- message{ message: "AddInput", payload: network.inputs[number], reply: ch}
+    _ = <-ch
 }
 
 func (network *Network) Input(number int) *Connection {
@@ -136,12 +164,12 @@ func (network *Network) Output(number int) *Connection {
 }
 
 
-func (network *Network) CreateNeuron(name string, 
-                                     inputs int, threshold Data) *Neuron {
+func (network *Network) CreateNeuron(name string, threshold Data) *Neuron {
 
+    network.debug("\tCreating %v\n", name)
     network.command[name] = make( chan message, NCPU)
     
-    n := newNeuron(name,  new(vector.Vector), network.command[name], inputs, threshold)
+    n := newNeuron(name,  new(vector.Vector), network.command[name], threshold)
 
     defer network.addNeuron(n)
     return &Neuron{command: network.command[name], input: n.input }
@@ -181,16 +209,47 @@ func neuronLoop(neuron *neuron) {
                             neuron.trace("adding output %v", command.payload)
                             neuron.outputs.Push(command.payload)
                             command.reply <- "Ok"
+                          case "AddInput":
+                            neuron.inputs_waiting++
+                            neuron.inputs++
+                            neuron.trace("input_wating is %v inputs is %v", neuron.inputs_waiting, neuron.inputs)
+                            command.reply <- "Ok"
                           case "Reset":
                             neuron.inputs = neuron.inputs_waiting
                             neuron.accumulator = 0
-                            command.reply <- "Ok" 
+                            command.reply <- "Ok"
+                          case "SetLogLevel":
+                            neuron.log_level = command.payload.(int)
+                            command.reply <- "Ok"
+                          default:
+                            if command.reply != nil {
+                                command.reply <- fmt.Sprintf("%v Error: Unknown command: %v", neuron.name, command.message)
+                            } else {
+                                fmt.Printf("%v Error: Unknown command: %v", neuron.name, command.message)
+                            }
                       }
 
         }
         }
     } 
 
+func (network *Network) info(format string, a ...interface{}) {
+    log_message(network.log_level, 1, format, a...)
+}
+
+func (network *Network) debug(format string, a ...interface{}) {
+    log_message(network.log_level, 2, format, a...)
+}
+
+func (network *Network) trace (format string, a ...interface{}) {
+    log_message(network.log_level, 3, format, a...)
+}
+
+func log_message(log_level int, message_level int, format string, a...interface{}) {
+    if log_level >= message_level {
+        fmt.Printf(format,  a...)
+    }
+}
 
 func (neuron *Neuron) AddOutput(conn *Connection ) {
     ch := make(chan string)
@@ -199,17 +258,38 @@ func (neuron *Neuron) AddOutput(conn *Connection ) {
     _ = <-ch
 }
 
-// a little backwards maybe 
+// Add a neuron as an input with weight 
 func (neuron *Neuron) AddInput(input *Neuron, w Data) {
     input.AddOutput(&Connection {c: neuron.input, weight: w })
+    ch := make(chan string)
+    neuron.command <- message{ message: "AddInput", payload: input, reply: ch}
+    _ = <-ch
 }
 
+func (neuron *neuron) LogLevel() int {
+    return neuron.log_level
+}
+
+func (neuron *Neuron) SetLogLevel(level int)  {
+
+    ch := make(chan string)
+    neuron.command <- message{ message: "SetLogLevel", payload: level, reply: ch}
+    _ = <-ch
+}
+
+func (neuron *neuron) info (format string, a ...interface{}) {
+        real_format := fmt.Sprintf("\t\t%v: %v\n", neuron.name, format)
+        log_message(neuron.log_level, 1, real_format,  a...)
+}
+
+func (neuron *neuron) debug(format string, a ...interface{}) {
+        real_format := fmt.Sprintf("\t\t%v: %v\n", neuron.name, format)
+        log_message(neuron.log_level, 2, real_format,  a...)
+}
 
 func (neuron *neuron) trace(format string, a ...interface{}) {
-    if neuron.log_level > 5 {
         real_format := fmt.Sprintf("\t\t%v: %v\n", neuron.name, format)
-        fmt.Printf(real_format,  a...)
-    }
+        log_message(neuron.log_level, 3, real_format,  a...)
 }
 
 
